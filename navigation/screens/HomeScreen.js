d40f0e5e3cd6ddc73963e57edf21bacb7a717a8b
Daniel Kang <dkang.jh@gmail.com>
2024-03-20 15:55:00 -0400

    added scrollable items

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,15 +1,27 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
 export default function HomeScreen({ navigation }) {
+  const data = [
+    { key: "1", name: "Item 1" },
+    { key: "2", name: "Item 2" },
+    { key: "3", name: "Item 3" },
+    // Add more items as needed
+  ];
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => alert("This is the Home screen.")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Home Screen
-      </Text>
+    <View
+      style={{
+        marginTop: 100,
+        flex: 5,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 }
